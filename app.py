0642b895f99803c38874e1794d354cbd8ddb6ce7
mengwaichan <chanmengwai1@gmail.com>
2022-12-07 03:52:57 -0500

    added new html files

--- app.py
+++ app.py
@@ -7,12 +7,21 @@ def index():
     return render_template('index.html')
 
 @app.route('/search')
-def login():
-    return render_template("product.html")
+def search():
+    return render_template("search.html")
     
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
